2bde243753a13ef4ce4f41083e6c2753f65c2570
Rozen2007 <deedirozen@gmail.com>
2022-02-26 06:51:28 +0530

    better ui v0.2

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./Home.css";
 import GitHubIcon from "@material-ui/icons/GitHub";
 
+
+
+
+
 function Home() {
+
   return (
     <div className="Home">
       <div className="box">
@@ -19,8 +24,24 @@ function Home() {
             <GitHubIcon />
           </a>
         </header>
-        <div className='Container'>
-asjvoihsaovh
+        <div className="Container">
+          <h1 className="h1">
+            The most powerful tool to  convert your
+            <div className="text">
+              <a className="gradient">Text to handwriting</a>
+            </div>
+          </h1>
+          <p className="description">
+            Hate writing assigments? This tool will help you convert your text
+            to handwriting 😜
+          </p>
+          <div className="button_section">
+            <a>
+              <div className="button_container"> try now</div>
+            </a>
+          </div>
+       
+          
         </div>
       </div>
     </div>
